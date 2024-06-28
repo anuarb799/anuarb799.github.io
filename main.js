593177116bc3f268b9c8c0658e67aa45fe0a5869
anuarb799 <anuarb799@gmail.com>
2024-06-28 16:48:59 +0500

    Still not

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import { OrbitControls } from '../node_modules/three/examples/jsm/Addons.js';
-import { GLTFLoader } from '../node_modules/three/addons/loaders/GLTFLoader.js';
+import * as THREE from "/node_modules/three/build/three.module.js";
+import { OrbitControls } from "/node_modules/three/examples/jsm/Addons.js";
+import { GLTFLoader } from "/node_modules/three/addons/loaders/GLTFLoader.js";
 import getStarfield from "/creatingstars.js";
 
 const scene = new THREE.Scene();
